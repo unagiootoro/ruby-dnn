072441e701b0a15c93a38a684c740c9c50e50a39
unagiootoro <ootoro838861@outlook.jp>
2018-09-19 22:24:48 +0900

    'require' to 'require_relative'

--- lib/dnn.rb
+++ lib/dnn.rb
@@ -9,13 +9,13 @@ Xumo::SFloat.srand(rand(2**64))
 
 module DNN; end
 
-require "dnn/version"
-require "dnn/core/error"
-require "dnn/core/model"
-require "dnn/core/initializers"
-require "dnn/core/layers"
-require "dnn/core/activations"
-require "dnn/core/cnn_layers"
-require "dnn/core/rnn_layers"
-require "dnn/core/optimizers"
-require "dnn/core/util"
+require_relative "dnn/version"
+require_relative "dnn/core/error"
+require_relative "dnn/core/model"
+require_relative "dnn/core/initializers"
+require_relative "dnn/core/layers"
+require_relative "dnn/core/activations"
+require_relative "dnn/core/cnn_layers"
+require_relative "dnn/core/rnn_layers"
+require_relative "dnn/core/optimizers"
+require_relative "dnn/core/util"
