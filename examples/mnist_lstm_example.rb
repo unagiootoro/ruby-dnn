597f9fc6a48c69e35130fe5523c71767c5ed509b
unagiootoro <ootoro838861@outlook.jp>
2019-03-13 21:55:18 +0900

    Bug fix

--- examples/mnist_lstm_example.rb
+++ examples/mnist_lstm_example.rb
@@ -17,8 +17,8 @@ x_test = Numo::SFloat.cast(x_test).reshape(x_test.shape[0], 28, 28)
 x_train /= 255
 x_test /= 255
 
-y_train = DNN::Util.to_categorical(y_train, 10)
-y_test = DNN::Util.to_categorical(y_test, 10)
+y_train = DNN::Util.to_categorical(y_train, 10, Numo::SFloat)
+y_test = DNN::Util.to_categorical(y_test, 10, Numo::SFloat)
 
 model = Model.new
 
