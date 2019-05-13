b2d52533307f680d9b87110b17d2e3617d4cb508
unagiootoro <ootoro838861@outlook.jp>
2019-05-13 22:27:38 +0900

    Add Iris dataset

---
+++ examples/iris_example.rb
@@ -0,0 +1,33 @@
+require "dnn"
+require "dnn/lib/iris"
+require "numo/linalg/autoloader"
+
+include DNN::Layers
+include DNN::Activations
+include DNN::Optimizers
+include DNN::Losses
+Model = DNN::Model
+Iris = DNN::Iris
+
+x, y = Iris.load(true)
+x_train, y_train = x[0...100, true], y[0...100]
+x_test, y_test = x[100...150, true], y[100...150]
+
+x_train /= 255
+x_test /= 255
+
+y_train = DNN::Utils.to_categorical(y_train, 3, Numo::SFloat)
+y_test = DNN::Utils.to_categorical(y_test, 3, Numo::SFloat)
+
+model = Model.new
+
+model << InputLayer.new(4)
+
+model << Dense.new(64)
+model << ReLU.new
+
+model << Dense.new(3)
+
+model.compile(Adam.new, SoftmaxCrossEntropy.new)
+
+model.train(x_train, y_train, 1000, batch_size: 10, test: [x_test, y_test])
---
+++ lib/dnn/lib/iris.rb
@@ -0,0 +1,60 @@
+require "csv"
+require_relative "downloader"
+
+module DNN
+  class DNN_Iris_LoadError < DNN_Error; end
+
+  module Iris
+    URL_CSV = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+    # Iris-setosa
+    SETOSA = 0
+    # Iris-versicolor
+    VERSICOLOR = 1
+    # Iris-virginica
+    VIRGINICA = 2
+
+    def self.downloads
+      return if File.exist?(url_to_file_name(URL_CSV))
+      Downloader.download(URL_CSV)
+    end
+
+    def self.load(shuffle = false, shuffle_seed = rand(1 << 31))
+      downloads
+      csv_array = CSV.read(url_to_file_name(URL_CSV)).select { |a| a.length > 0 }
+      x = Numo::SFloat.zeros(csv_array.length, 4)
+      y = Numo::SFloat.zeros(csv_array.length)
+      csv_array.each.with_index do |(sepal_length, sepal_width, petal_length, petal_width, classes), i|
+        x[i, 0] = sepal_length.to_f
+        x[i, 1] = sepal_width.to_f
+        x[i, 2] = petal_length.to_f
+        x[i, 3] = petal_width.to_f
+        y[i] = case classes
+        when "Iris-setosa"
+          SETOSA
+        when "Iris-versicolor"
+          VERSICOLOR
+        when "Iris-virginica"
+          VIRGINICA
+        else
+          raise DNN_Iris_LoadError.new("Unknown class name '#{classes}' for iris")
+        end
+      end
+      if shuffle
+        orig_seed = Random::DEFAULT.seed
+        srand(shuffle_seed)
+        indexs = (0...csv_array.length).to_a.shuffle
+        x[indexs, true] = x
+        y[indexs] = y
+        srand(orig_seed)
+      end
+      [x, y]
+    end
+
+    private_class_method
+
+    def self.url_to_file_name(url)
+      __dir__ + "/" + url.match(%r`.+/(.+)$`)[1]
+    end
+  end
+end
