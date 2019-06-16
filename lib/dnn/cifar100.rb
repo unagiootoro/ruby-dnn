f068356bc02777bd92c426724437842d8fac2e1c
unagiootoro <ootoro838861@outlook.jp>
2019-06-16 21:08:31 +0900

    Support to CIFAR100

--- lib/dnn/cifar100.rb
+++ lib/dnn/cifar100.rb
@@ -3,34 +3,32 @@ require "archive/tar/minitar"
 require_relative "../../ext/cifar_loader/cifar_loader"
 require_relative "downloader"
 
-URL_CIFAR10 = "https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz"
-DIR_CIFAR10 = "cifar-10-batches-bin"
+URL_CIFAR100 = "https://www.cs.toronto.edu/~kriz/cifar-100-binary.tar.gz"
+DIR_CIFAR100 = "cifar-100-binary"
 
 module DNN
-  module CIFAR10
-    class DNN_CIFAR10_LoadError < DNN_Error; end
+  module CIFAR100
+    class DNN_CIFAR100_LoadError < DNN_Error; end
 
     def self.downloads
-      return if Dir.exist?(__dir__ + "/downloads/" + DIR_CIFAR10)
-      Downloader.download(URL_CIFAR10)
-      cifar10_binary_file_name = __dir__ + "/downloads/" + URL_CIFAR10.match(%r`.+/(.+)`)[1]
+      return if Dir.exist?(__dir__ + "/downloads/" + DIR_CIFAR100)
+      Downloader.download(URL_CIFAR100)
+      cifar100_binary_file_name = __dir__ + "/downloads/" + URL_CIFAR100.match(%r`.+/(.+)`)[1]
       begin
-        Zlib::GzipReader.open(cifar10_binary_file_name) do |gz|
+        Zlib::GzipReader.open(cifar100_binary_file_name) do |gz|
           Archive::Tar::Minitar::unpack(gz, __dir__)
         end
       ensure
-        File.unlink(cifar10_binary_file_name)
+        File.unlink(cifar100_binary_file_name)
       end
     end
 
     def self.load_train
       downloads
       bin = ""
-      (1..5).each do |i|
-        fname = __dir__ + "/downloads/#{DIR_CIFAR10}/data_batch_#{i}.bin"
-        raise DNN_CIFAR10_LoadError.new(%`file "#{fname}" is not found.`) unless File.exist?(fname)
-        bin << File.binread(fname)
-      end
+      fname = __dir__ + "/downloads/#{DIR_CIFAR100}/train.bin"
+      raise DNN_CIFAR100_LoadError.new(%`file "#{fname}" is not found.`) unless File.exist?(fname)
+      bin << File.binread(fname)
       x_bin, y_bin = CIFAR.load_binary(bin, 50000)
       x_train = Numo::UInt8.from_binary(x_bin).reshape(50000, 3, 32, 32).transpose(0, 2, 3, 1).clone
       y_train = Numo::UInt8.from_binary(y_bin)
@@ -39,8 +37,8 @@ module DNN
 
     def self.load_test
       downloads
-      fname = __dir__ + "/downloads/#{DIR_CIFAR10}/test_batch.bin"
-      raise DNN_CIFAR10_LoadError.new(%`file "#{fname}" is not found.`) unless File.exist?(fname)
+      fname = __dir__ + "/downloads/#{DIR_CIFAR100}/test.bin"
+      raise DNN_CIFAR100_LoadError.new(%`file "#{fname}" is not found.`) unless File.exist?(fname)
       bin = File.binread(fname)
       x_bin, y_bin = CIFAR.load_binary(bin, 10000)
       x_test = Numo::UInt8.from_binary(x_bin).reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).clone
