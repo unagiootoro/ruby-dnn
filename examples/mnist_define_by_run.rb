7188f1fc02383e176b400342c880b3822d12f54b
unagiootoro <ootoro838861@outlook.jp>
2019-07-07 23:43:26 +0900

    Add define by run example

---
+++ examples/mnist_define_by_run.rb
@@ -0,0 +1,52 @@
+require "dnn"
+require "dnn/mnist"
+# If you use numo/linalg then please uncomment out.
+# require "numo/linalg/autoloader"
+
+include DNN::Layers
+include DNN::Activations
+include DNN::Optimizers
+include DNN::Losses
+include DNN::Models
+MNIST = DNN::MNIST
+
+x_train, y_train = MNIST.load_train
+x_test, y_test = MNIST.load_test
+
+x_train = Numo::SFloat.cast(x_train).reshape(x_train.shape[0], 784)
+x_test = Numo::SFloat.cast(x_test).reshape(x_test.shape[0], 784)
+
+x_train /= 255
+x_test /= 255
+
+y_train = DNN::Utils.to_categorical(y_train, 10, Numo::SFloat)
+y_test = DNN::Utils.to_categorical(y_test, 10, Numo::SFloat)
+
+class MLP < Model
+  def initialize
+    super
+    @l1 = Dense.new(256)
+    @l2 = Dense.new(256)
+    @l3 = Dense.new(10)
+    @bn1 = BatchNormalization.new
+    @bn2 = BatchNormalization.new
+  end
+
+  def call(x, learning_phase)
+    x = InputLayer.new(784).(x)
+    x = @l1.(x)
+    x = @bn1.(x, learning_phase)
+    x = ReLU.(x)
+    x = @l2.(x)
+    x = @bn2.(x, learning_phase)
+    x = ReLU.(x)
+    x = @l3.(x)
+    x
+  end
+end
+
+model = MLP.new
+
+model.setup(Adam.new, SoftmaxCrossEntropy.new)
+
+model.train(x_train, y_train, 10, batch_size: 100, test: [x_test, y_test])
