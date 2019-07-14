cfbc5ccc284f84170eba0c8b304481c4cee797ff
unagiootoro <ootoro838861@outlook.jp>
2019-07-14 15:33:50 +0900

    Refactoring

--- lib/dnn/core/param.rb
+++ lib/dnn/core/param.rb
@@ -1,9 +1,11 @@
-class DNN::Param
-  attr_accessor :data
-  attr_accessor :grad
+module DNN
+  class Param
+    attr_accessor :data
+    attr_accessor :grad
 
-  def initialize(data = nil, grad = nil)
-    @data = data
-    @grad = grad
+    def initialize(data = nil, grad = nil)
+      @data = data
+      @grad = grad
+    end
   end
 end
