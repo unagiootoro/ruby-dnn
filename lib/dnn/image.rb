d89cd8ef8a854e0fc901f1579d719282646b6276
unagiootoro <ootoro838861@outlook.jp>
2019-07-15 21:39:34 +0900

    Add error handling process

--- lib/dnn/image.rb
+++ lib/dnn/image.rb
@@ -11,6 +11,16 @@ module DNN
     end
 
     def self.write(file_name, img, quality: 100)
+      _write(file_name, img, quality: quality)
+    rescue Errno::ENOENT => ex
+      dir_name = file_name.match(%r`(.*)/.+$`)[1]
+      Dir.mkdir(dir_name)
+      _write(file_name, img, quality: quality)
+    rescue => ex
+      raise Image::WriteError.new(ex.message)
+    end
+
+    def self._write(file_name, img, quality: 100)
       if img.shape.length == 2
         img = Numo::UInt8[img, img, img].transpose(1, 2, 0).clone
       elsif img.shape[2] == 1
@@ -28,8 +38,6 @@ module DNN
       when /\.jpg$/i, /\.jpeg/i
         Stb.stbi_write_jpg(file_name, w, h, ch, bin, quality)
       end
-    rescue => ex
-      raise Image::WriteError.new(ex.message)
     end
   end
 
