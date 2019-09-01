eae73b47be182d8968815daf45f820168b6d4852
unagiootoro <ootoro838861@outlook.jp>
2019-09-01 14:50:39 +0900

    Change handling of Last batch

--- test/iterator_test.rb
+++ test/iterator_test.rb
@@ -10,8 +10,8 @@ class TestIterator < MiniTest::Unit::TestCase
 
     iter.next_batch(7)
     x, y = iter.next_batch(7)
-    assert_equal [7, 10], x.shape
-    assert_equal [7, 10], y.shape
+    assert_equal [3, 10], x.shape
+    assert_equal [3, 10], y.shape
   end
 
   def test_next_batch2
@@ -21,5 +21,16 @@ class TestIterator < MiniTest::Unit::TestCase
     iter.next_batch(7)
     x, * = iter.next_batch(7)
     assert_equal Numo::SFloat[7, 8, 9], x.flatten
+  end
+
+  def test_foreach
+    x_datas = Numo::Int32.new(10, 1).seq
+    iter = DNN::Iterator.new(x_datas, x_datas, random: false)
+
+    cnt = 0
+    iter.foreach(3) do |x_batch, y_batch|
+      cnt += 1
+    end
+    assert_equal 4, cnt
   end
 end
