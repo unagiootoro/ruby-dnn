69ef19077243c6f975b02feb5be83b1be483fc0f
unagiootoro <ootoro838861@outlook.jp>
2020-01-12 21:49:49 +0900

    Refactoring

--- examples/vae.rb
+++ examples/vae.rb
@@ -9,16 +9,10 @@ include DNN::Optimizers
 include DNN::Losses
 
 x_train, y_train = DNN::MNIST.load_train
-x_test, y_test = DNN::MNIST.load_test
 
 x_train = Numo::SFloat.cast(x_train).reshape(x_train.shape[0], 784)
-x_test = Numo::SFloat.cast(x_test).reshape(x_test.shape[0], 784)
 
 x_train /= 255
-x_test /= 255
-
-y_train = DNN::Utils.to_categorical(y_train, 10, Numo::SFloat)
-y_test = DNN::Utils.to_categorical(y_test, 10, Numo::SFloat)
 
 $z_dim = 2
 $z_mean = nil
