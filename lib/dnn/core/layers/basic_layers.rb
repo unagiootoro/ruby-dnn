50a61b3298928861d2488c7d2e7009697f1d0775
unagiootoro <ootoro838861@outlook.jp>
2020-02-22 22:35:40 +0900

    Link class to unified

--- lib/dnn/core/layers/basic_layers.rb
+++ lib/dnn/core/layers/basic_layers.rb
@@ -6,7 +6,7 @@ module DNN
         x = input.data
         prev = (input.is_a?(Tensor) ? input.link : input)
         y = forward_node(x)
-        link = Link.new(prev, self)
+        link = Link.new([prev], self)
         prev.next = link if prev.is_a?(Link)
         Tensor.convert(y, link)
       end
--- lib/dnn/core/link.rb
+++ lib/dnn/core/link.rb
@@ -1,35 +1,11 @@
 module DNN
   class Link
-    attr_accessor :prev
+    attr_accessor :prevs
     attr_accessor :next
     attr_accessor :layer_node
 
-    def initialize(prev = nil, layer_node = nil)
-      @prev = prev
-      @layer_node = layer_node
-      @next = nil
-    end
-
-    def forward(x)
-      x = @layer_node.(x)
-      @next ? @next.forward(x) : x
-    end
-
-    def backward(dy = Xumo::SFloat[1])
-      dy = @layer_node.backward_node(dy)
-      @prev&.backward(dy)
-    end
-  end
-
-  class TwoInputLink
-    attr_accessor :prev1
-    attr_accessor :prev2
-    attr_accessor :next
-    attr_accessor :layer_node
-
-    def initialize(prev1 = nil, prev2 = nil, layer_node = nil)
-      @prev1 = prev1
-      @prev2 = prev2
+    def initialize(prevs = nil, layer_node = nil)
+      @prevs = prevs
       @layer_node = layer_node
       @next = nil
       @hold = []
@@ -37,7 +13,7 @@ module DNN
 
     def forward(x)
       @hold << x
-      return if @hold.length < 2
+      return if @hold.length < @prevs.length
       x = @layer_node.(*@hold)
       @hold = []
       @next ? @next.forward(x) : x
@@ -46,12 +22,12 @@ module DNN
     def backward(dy = Xumo::SFloat[1])
       dys = @layer_node.backward_node(dy)
       if dys.is_a?(Array)
-        dy1, dy2 = *dys
+        dys.each.with_index do |dy, i|
+          @prevs[i]&.backward(dy)
+        end
       else
-        dy1 = dys
+        @prevs.first&.backward(dys)
       end
-      @prev1&.backward(dy1)
-      @prev2&.backward(dy2) if dy2
     end
   end
 end
